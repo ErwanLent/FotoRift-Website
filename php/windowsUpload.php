c21f181947dcdffb80e2e6f60c2984cd1ffbe7f7
ErwanLent <erwanlent@gmail.com>
2014-01-04 10:04:57 -0800

    Updated to mirror bitbucket repo.

---
+++ php/windowsUpload.php
@@ -0,0 +1,15 @@
+<?php
+ 	
+	// PHP Development By Alberto Villalobos
+	// http://albertovillalobos.me/
+	
+	$encodedImage = $_POST["data"];
+    $unencodedData=base64_decode($encodedImage)or die("Error decoding image.");
+    $name = uniqid();
+ 
+    $fp = fopen( "../uploads/$name.png", "wb" )or die("Error opening uploads folder.");
+    fwrite( $fp, $unencodedData)or die("Error writing file.");
+    fclose( $fp )or die("Error closing file.");
+ 
+    echo "http://erwanlent.com/uploads/$name.png";
+?>
